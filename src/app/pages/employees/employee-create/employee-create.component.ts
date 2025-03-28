7a09b7a92f3ffb3be724a150c381724ced5635dd
Timothy jan <jan.timothy@gmail.com>
2025-03-28 12:28:05 -0700

    Employee Create and Employee List Complete

---
+++ src/app/pages/employees/employee-create/employee-create.component.ts
@@ -0,0 +1,79 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Department } from 'src/app/models/department.model';
+import { Role } from 'src/app/models/role.model';
+import { DepartmentService } from 'src/app/services/department.service';
+import { EmployeeService } from 'src/app/services/employee.service';
+import { RoleService } from 'src/app/services/role.service';
+import { IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonInput, IonButton, IonSelect, IonSelectOption } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-employee-create',
+  templateUrl: './employee-create.component.html',
+  styleUrls: ['./employee-create.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent,
+    IonInput,
+    IonButton,
+    IonSelect,
+    IonSelectOption
+  ]
+})
+export class EmployeeCreateComponent implements OnInit {
+
+  departments: Department[] = []; // Array to hold department data
+  roles: Role[] = []; // Array to hold role data
+
+  employeeForm: FormGroup = new FormGroup({
+    name: new FormControl("", [Validators.required, Validators.minLength(2), Validators.maxLength(100)]),
+    salary: new FormControl(0,  Validators.min(0)),
+    departmentID: new FormControl(-1, Validators.required),
+    roleID: new FormControl(null, Validators.required)
+  });
+
+  constructor(
+    private _employeeService: EmployeeService,
+    private _departmentService: DepartmentService,
+    private _roleService: RoleService
+  ) {}
+
+  ngOnInit(): void {
+    this.departments = this._departmentService.getDepartments();
+
+    // Listen to changes on departmentID and update roles accordingly
+    // this.departmentSelectionChange();
+  }
+
+  /** Department change updates the roles array to the selected Department Roles  */
+  departmentSelectionChange(event: CustomEvent): void {
+    this.getRolesFromDepartmentID(event.detail.value);
+  }
+
+  /** Roles array is updated to selected departmentID Department roles */
+  getRolesFromDepartmentID(departmentID: number): void {
+    this.roles = this._roleService.getRolesFromDepartmentID(departmentID);
+  }
+
+  onSubmit(): void {
+    if (this.employeeForm.valid) {
+      const formValue = {
+        ...this.employeeForm.value,
+        departmentID: Number(this.employeeForm.value.departmentID),
+        roleID: Number(this.employeeForm.value.roleID),
+      }
+      // console.log('Form Submitted:', formValue);
+      this._employeeService.addEmployee(formValue);
+      this.employeeForm.reset();
+      this._employeeService.notifyEmployeesChanged();
+    }
+  }
+
+}
---
+++ src/app/pages/employees/employee-list/employee-list.component.ts
@@ -0,0 +1,125 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Department } from 'src/app/models/department.model';
+import { Employee } from 'src/app/models/employee.model';
+import { Role } from 'src/app/models/role.model';
+import { DepartmentService } from 'src/app/services/department.service';
+import { EmployeeService } from 'src/app/services/employee.service';
+import { RoleService } from 'src/app/services/role.service';
+import { IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonList, IonItem, IonButton, IonIcon, IonGrid, IonRow, IonCol, ActionSheetController, ModalController} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { createSharp, trashSharp, saveSharp, ellipsisVerticalSharp } from 'ionicons/icons';
+import { EmployeeEditModalComponent } from '../employee-edit-modal/employee-edit-modal.component';
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.scss'],
+  standalone: true,
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent,
+    IonList,
+    IonItem,
+    IonButton,
+    IonIcon,
+    IonGrid,
+    IonRow,
+    IonCol,
+  ]
+})
+export class EmployeeListComponent implements OnInit {
+
+  employees: Employee[] = [];
+  departments: Department[] = [];
+  roles: Role[] = [];
+
+  constructor(
+    private _employeeService: EmployeeService,
+    private _departmentService: DepartmentService,
+    private _roleService: RoleService,
+    private actionSheetCtrl: ActionSheetController,
+    private modalCtrl: ModalController
+  ) {
+    addIcons({ createSharp, trashSharp, saveSharp, ellipsisVerticalSharp });
+  }
+
+  ngOnInit(): void {
+    this.loadEmployees();
+    this.departments = this._departmentService.getDepartments();
+    this.roles = this._roleService.getRoles();
+  }
+
+  /** Load all Employees */
+  loadEmployees(): void {
+    this.employees = this._employeeService.getEmployees();
+  }
+
+  /** Get Department name from DepartmentID */
+  getDepartmentName(departmentID: number): string | undefined {
+    return this._departmentService.getDepartment(departmentID)?.departmentName;
+  }
+
+  /** Get Role name from RoleID */
+  getRoleName(roleID: number): string | undefined {
+    return this._roleService.getRole(roleID)?.roleName;
+  }
+
+  /** Action Sheet Controller */
+  async presentEmployeeActionSheet(employee: Employee) {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: employee.name,
+      buttons: [
+        {
+          text: 'Edit',
+          handler: () => this.openEmployeeEditModal(employee.employeeID),
+        },
+        {
+          text: 'Delete',
+          role: 'destructive',
+          handler: () => this.onDelete(employee.employeeID),
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          data: {
+            action: 'cancel',
+          },
+        },
+      ],
+    });
+    await actionSheet.present();
+  }
+
+  /** Opens Employee Edit Modal */
+  async openEmployeeEditModal(employeeID: number) {
+    const modal = await this.modalCtrl.create({
+      component: EmployeeEditModalComponent,
+      componentProps: {
+        employeeID: employeeID
+      }
+    });
+    modal.present();
+
+    const { data, role } = await modal.onWillDismiss();
+
+    if (role === 'confirm') {
+      console.log(data, role);
+    }
+  }
+
+  /** Deletes Employee */
+  onDelete(employeeID: number): void {
+    const confirmDelete = confirm('Are you sure you want to delete this employee?');
+    if (confirmDelete) {
+      this._employeeService.deleteEmployee(employeeID);
+      this.loadEmployees();
+    }
+  }
+
+}
